24e1a4a1bcb0f47330e67bbd24196437a5f7d28d
etilelab <taegyeong7202@gmail.com>
2021-09-17 11:02:21 +0900

    class dense layer, row vectors

--- Class01/class01_affinefunc02.py
+++ Class01/class01_affinefunc02.py
@@ -6,7 +6,8 @@ print(x.shape,'\n',x)
 
 dense = Dense(units=1)  # W를 몇개 만들어야 되는지 모르는 상태
 
-y_tf = dense(x) # W 10개, B1개 초기화해야되는구나 !
+y_tf = dense(x) # 통과할때, W 10개, B 1개 초기화해야되는구나 !
+
 W, B = dense.get_weights()  # 초기화
 y_man = tf.linalg.matmul(x,W) + B
 
